da20854652103f7290bb3f6afe428be63028e587
DU0K <svenbras09@gmail.com>
2025-12-09 12:59:31 +0100

    commit

---
+++ list/Remove/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Remove;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.run();
+    }
+
+    internal void run()
+    {
+        List<double>reviews = new List<double>() {4.5, 3.0, 551.0, 2.5,};
+        reviews.Remove(551.0);
+
+        reviews.RemoveAt(1);
+
+        foreach (var review in reviews)
+        {
+            Console.WriteLine(review);
+        }
+    }
+}
